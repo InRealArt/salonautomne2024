7a59cb581b7130cd446f24417a0c56a47ed97091
gilbruno <gilles_bruno@gmx.fr>
2024-10-21 17:01:48 +0200

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,17 +6,11 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.socialIcons}>
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/inrealartgallery/" target="_blank" rel="noopener noreferrer">
           <Image src="/img/footer/instagram.jpg" alt="Instagram" width={24} height={24} />
         </a>
-        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-          <Image src="/img/footer/facebook.jpg" alt="Facebook" width={24} height={24} />
-        </a>
-        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://x.com/InRealArt" target="_blank" rel="noopener noreferrer">
           <Image src="/img/footer/twitter.jpg" alt="Twitter" width={24} height={24} />
-        </a>
-        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-          <Image src="/img/footer/youtube.jpg" alt="YouTube" width={24} height={24} />
         </a>
       </div>
     </footer>
